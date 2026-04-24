0d45d79ff08a5df6c474988ef56ed4ebda6a3a0d
PrajwalLuitel <prajwalluitel@gmail.com>
2026-04-24 16:49:24 -0400

    production issue fix try

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         port: "8000",
         pathname: "/uploads/**",
       },
+      {
+        protocol: "https",
+        hostname: "meeting.davidsklar.com",
+        pathname: "/uploads/**",
+      },
     ],
   },
 };
